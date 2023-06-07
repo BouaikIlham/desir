e2ae6965faac15d65c48e90bf3e8439651655799
ilham <ibouaik@gmail.com>
2023-06-07 12:08:35 +0100

    add category to rental model

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Modal from "./Modal";
 import UseRentalModal from "@/app/hooks/UseRentalModal";
-
-
+import {useState } from "react";
+import { useForm } from "react-hook-form";
+import Heading from "../Heading";
+import CategoryInput from "../inputs/CategoryInput";
+import {categories} from "../navbar/Categories";
 enum STEPS {
     CATEGORY = 0,
     LOCATION = 1,
@@ -12,13 +15,43 @@ enum STEPS {
     PRICE = 5,
 }
 const RentModal = () => {
-   
+    const [step, setStep] = useState(STEPS.CATEGORY)
+    const [isLoading, setIsLoading] = useState(false)
     const rentalModal = UseRentalModal()
 
+    const { register, watch, formState: { errors }, handleSubmit, setValue} = useForm({
+        defaultValues: {
+            category: "",
+            location: null,
+            imageSrc: "",
+            price: 1,
+            title: "",
+            description: "",
+        }
+    });
+    const category = watch('category')
+    const location = watch('location')
+    const imageSrc = watch('imageSrc')
+
     const bodyContent = (
-        <div>
-            <h1>bodyContent</h1>
-        </div>
+       <div className="flex flex-col gap-8">
+            <Heading
+                title="what of these describes your car ?"
+                subtitle="pick a category"
+            />
+            <div className="grid grid-col-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+               {categories.map((category) => (
+                <div key={category.label} className="col-span-1">
+                    <CategoryInput
+                        label={category.label}
+                        icon={category.icon}
+                        onClick={() => {}}
+                        selected
+                    />
+                </div>
+               ))}
+            </div>
+       </div>
     )
     const footerContent = (
         <div>
