efe05b985dec8c941de1c447565a87edb89263ee
ilham <ibouaik@gmail.com>
2023-06-10 18:58:13 +0100

    car client component

--- app/cars/[carId]/page.tsx
+++ app/cars/[carId]/page.tsx
@@ -1,12 +1,23 @@
 import getCarById from "@/app/actions/getCarById"
-
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import EmptyState from "@/app/components/EmptyState";
+import CarClient from "./CarClient";
+import Navbar from "@/app/components/navbar/Navbar";
 interface IParams {
     carId?: string;
 }
 const page = async ({params} : {params: IParams}) => {
     const car = await getCarById(params)
+    const currentUser = await getCurrentUser()
+
+    if (!car) {
+        <EmptyState />
+    }
   return (
-    <div>car id</div>
+    <>
+        <Navbar />
+        <CarClient />
+    </>
   )
 }
 
