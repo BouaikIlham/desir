15b9d1e02591b926c4a9e8cdc6d13a5ae1b81a04
ilham <ibouaik@gmail.com>
2023-06-23 12:23:25 +0100

    user client componenet

--- app/user/UserClient.tsx
+++ app/user/UserClient.tsx
@@ -1,7 +1,43 @@
 'use client'
-const UserClient = () => {
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import CarCard from "../components/cars/CarCard";
+import { SafeCar, SafeUser } from "../types"
+
+interface UserClientProps {
+  currentUser: SafeUser | null;
+  cars: SafeCar[];
+}
+const UserClient: React.FC<UserClientProps> = ({
+  currentUser,
+  cars
+}) => {
   return (
-    <div>UserClient</div>
+    <Container>
+       <Heading
+        title="Favorites"
+        subtitle="List of cars you have favorited!"
+      />
+      <div className="mt-10
+                      grid
+                      gap-4
+                      grid-cols-1
+                      sm:grid-cols-2
+                      md:grid-cols-3
+                      lg:grid-cols-4
+                      xl:grid-cols-5
+                      2xl:grid-cols-6
+                    ">
+        {cars.map((car) => (
+          <CarCard
+            key={car.id}
+            data={car}
+            currentUser={currentUser}
+
+          />
+        ))}
+      </div>
+    </Container>
   )
 }
 
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -3,14 +3,43 @@ import Container from '../components/Container'
 import Navbar from '../components/navbar/Navbar'
 import getCurrentUser from '../actions/getCurrentUser'
 import getFavoriteCars from '../actions/getFavoriteCars'
+import UserClient from './UserClient'
+import EmptyState from '../components/EmptyState'
 const page = async () => {
   const currentUser = await getCurrentUser()
-  const favorites = await getFavoriteCars()
+  const cars = await getFavoriteCars()
 
+  if (!currentUser) {
+    return (
+      <>
+        <Navbar currentUser={currentUser} />
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please Login"
+      />
+      </>
+    )
+  }
+
+  if (cars.length === 0) {
+    return (
+     <>
+        <Navbar currentUser={currentUser} />
+        <EmptyState
+          title="No favorite found"
+          subtitle="Looks like you have no favorites on your properties"
+      />
+     </>
+    )
+  }
 
   return (
     <Container>
         <Navbar currentUser={currentUser} />
+        <UserClient
+          currentUser = {currentUser}
+          cars= {cars}
+        />
     </Container>
 
   )
